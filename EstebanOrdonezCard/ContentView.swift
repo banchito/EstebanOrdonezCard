62764b0daa6aee4cf4d2a9f9e31f99e62326d2cd
Esteban Ordonez <estebanusco@gmail.com>
2020-02-28 10:50:05 -0500

    Create a SwiftUI layout app with a Business Card

--- EstebanOrdonezCard/ContentView.swift
+++ EstebanOrdonezCard/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  EstebanOrdonezCard
+//  EstebanCard
 //
-//  Created by Esteban Ordonez on 2/28/20.
+//  Created by Esteban Ordonez on 2/26/20.
 //  Copyright © 2020 Esteban Ordonez. All rights reserved.
 //
 
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("Esteban")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 10)
+                )
+                Text("Esteban Ordonez")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+1 123 455 609", imageName: "phone.fill")
+                InfoView(text: "Estebanusco@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
